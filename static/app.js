e1e7ad891e673fa07ceea0b547adcc03ca35dbf4
Nishiyama Yuya <nsy@ulz.nu>
2010-10-19 02:19:39 +0900

    ported socket.io sample to (fab).

---
+++ static/app.js
@@ -0,0 +1,30 @@
+function message(obj){
+  var el = document.createElement('p');
+  if ('announcement' in obj) el.innerHTML = '<em>' + esc(obj.announcement) + '</em>';
+  else if ('message' in obj) el.innerHTML = '<b>' + esc(obj.message[0]) + ':</b> ' + esc(obj.message[1]);
+  document.getElementById('chat').appendChild(el);
+  document.getElementById('chat').scrollTop = 1000000;
+}
+
+function send(){
+  var val = document.getElementById('text').value;
+  socket.send(val);
+  message({ message: ['you', val] });
+  document.getElementById('text').value = '';
+}
+
+function esc(msg){
+  return msg.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+};
+
+var socket = new io.Socket(null, {port: 80});
+socket.connect();
+socket.on('message', function(obj){
+  if ('buffer' in obj){
+    document.getElementById('form').style.display='block';
+    document.getElementById('chat').innerHTML = '';
+    
+    for (var i in obj.buffer) message(obj.buffer[i]);
+  } else message(obj);
+});
+j
